1415ad51051ce3917e15a6609f26e0700c043a5d
Martin Lindström <martinllindstroem@gmail.com>
2021-03-19 15:36:46 +0100

    updated header

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -2,6 +2,7 @@
 /**
  * Supply the basis for the navbar as an array.
  */
+$user = isset($_SESSION["user"]) ? $_SESSION["user"] : null;
 return [
     // Use for styling the menu
     "wrapper" => null,
@@ -12,41 +13,28 @@ return [
         [
             "text" => "Hem",
             "url" => "",
-            "title" => "Första sidan, börja här.",
-        ],
-        [
-            "text" => "Redovisning",
-            "url" => "redovisning",
-            "title" => "Redovisningstexter från kursmomenten.",
-            "submenu" => [
-                "items" => [
-                    [
-                        "text" => "Kmom01",
-                        "url" => "redovisning/kmom01",
-                        "title" => "Redovisning för kmom01.",
-                    ],
-                    [
-                        "text" => "Kmom02",
-                        "url" => "redovisning/kmom02",
-                        "title" => "Redovisning för kmom02.",
-                    ],
-                ],
-            ],
+            "title" => "",
         ],
         [
             "text" => "Om",
             "url" => "om",
-            "title" => "Om denna webbplats.",
+            "title" => "",
         ],
         [
-            "text" => "Styleväljare",
-            "url" => "style",
-            "title" => "Välj stylesheet.",
+            "text" => "Questions",
+            "url" => "questions",
+            "title" => "",
         ],
         [
-            "text" => "Verktyg",
-            "url" => "verktyg",
-            "title" => "Verktyg och möjligheter för utveckling.",
+            "text" => "Tags",
+            "url" => "tags",
+            "title" => "",
         ],
+        // [
+        //     "text" => "Sign up",
+        //     "url" => "user/register",
+        //     "title" => "Sign up",
+        // ],
+        ($user ? ["text" => "Profile", "url" => "user/profile/{$user}", "title" => "Profile"] : ["text" => "Sign up", "url" => "user/register", "title" => "Sign up"])
     ],
 ];
